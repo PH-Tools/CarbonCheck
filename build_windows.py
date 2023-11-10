32e11da7b47b479a8e50451efcc2183b6b18368e
PH Tools <phtools@bldgtyp.com>
2023-11-10 17:26:16 +0100

    Update build-config files

--- build_windows.py
+++ build_windows.py
@@ -1,30 +1,50 @@
-"""cx_Freeze Build configuration for Windows MSI Installer.
-
-NOTE: be sure to --upgrade all dependencies first
-"pip install -r requirements.txt --upgrade"
-"python build_windows.py bdist_msi"
 """
+cx_Freeze Build configuration for Windows MSI Installer.
 
+The build is designed to be run from a GitHub Action (.github/workflows/build.yml) and will
+automatically rebuild the application when triggered. It will create the .msi installer file within 
+a "dist" folder, and add it as an artifact to the GitHub repo.
+
+Most of the attributes (version, name) are read in from the pyproject.toml file.
+
+For Testing only, from command-line:
+NOTE: Be sure to update all dependencies before building.
+
+>> # Build .MSI
+>> python build_windows.py bdist_msi
+
+>> # Build .EXE
+>> python build_windows.py build
+"""
 
 import sys
 import os
 from pathlib import Path
+import toml
 from cx_Freeze import setup, Executable
 
+# -----------------------------------------------------------------------------
+# Read the pyproject.toml file with then various build options / attributes
+pyproject = toml.load("pyproject.toml")["tool"]["cx_Freeze"]
 
-def find_data_file(filename):
+
+# -----------------------------------------------------------------------------
+def find_data_file(filename) -> str:
     if getattr(sys, "frozen", False):
         # The application is frozen
-        datadir = os.path.dirname(sys.executable)
+        data_dir = os.path.dirname(sys.executable)
     else:
         # The application is not frozen
         # Change this bit to match where you store your data files:
-        datadir = os.path.dirname(__file__)
-    return os.path.join(datadir, filename)
+        data_dir = os.path.dirname(__file__)
+    return os.path.join(data_dir, filename)
 
 
-# Add in all the required dependencies.
-# Automatic detection should not be relied upon
+ICON_FILE = find_data_file(Path("CC_GUI", "resources", "logo_CarbonCheck.ico").resolve())
+
+
+# -----------------------------------------------------------------------------
+# Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
     "includes": [
         "ladybug",
@@ -42,6 +62,7 @@ build_exe_options = {
         "honeybee_ph_utils",
         "honeybee_energy_ph",
         "PHX",
+        "toml",
         "yaml",
         "lxml",
         "pdfplumber",
@@ -54,29 +75,37 @@ build_exe_options = {
     "include_msvcr": True,
 }
 
+
+# -----------------------------------------------------------------------------
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
 
+
+# -----------------------------------------------------------------------------
+# Get the options for the .MSI file created
 bdist_msi_options = {
-    "add_to_path": False,
-    "initial_target_dir": "C:\\CarbonCheck",
+    "add_to_path": pyproject["bdsist"]["msi"]["add_to_path"],
+    "initial_target_dir": pyproject["bdsist"]["msi"]["initial_target_dir"],
+    "target_name": pyproject["bdsist"]["msi"]["target_name"],
+    "target_version": pyproject["bdsist"]["msi"]["target_version"],
 }
 
-icon_file = find_data_file(Path("CC_GUI", "resources", "logo_CarbonCheck.ico").resolve())
+
+# -----------------------------------------------------------------------------
 setup(
-    name="CarbonCheck",
-    version="0.1",
-    description="Passive House Model Baseliner and Reporting.",
+    name=pyproject["app"]["name"],
+    version=pyproject["bdsist"]["msi"]["target_version"],
+    description=pyproject["app"]["description"],
     options={
         "build_exe": build_exe_options,
         "bdist_msi": bdist_msi_options,
     },
     executables=[
         Executable(
-            "CarbonCheck.py",
-            copyright="Copyright (C) 2023 Passive House Accelerator",
+            script="CarbonCheck.py",
+            copyright=pyproject["app"]["copyright"],
             base=base,
-            icon=icon_file,
+            icon=ICON_FILE,
         )
     ],
 )
